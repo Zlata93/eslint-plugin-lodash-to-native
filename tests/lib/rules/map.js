81f9da93efd01853a488568336c7002cb6e6d744
Zlata Kotlova <z.kotlova@informika.ru>
2019-09-24 19:04:24 +0300

    test

--- tests/lib/rules/map.js
+++ tests/lib/rules/map.js
@@ -1,2 +1,22 @@
 var rule = require("../../../lib/rules/map");
 var RuleTester = require("eslint").RuleTester;
+
+var ruleTester = new RuleTester();
+
+ruleTester.run('map', rule, {
+    valid: [
+        '_.map({ \'a\': 4, \'b\': 8 }, function(n) { return n*n; });',
+        '[1,2,3].map(function(n) { return n*n; });'
+    ],
+    invalid: [
+        {
+            code: '_.map([4, 8], function(n) { return n*n; });',
+            errors: [
+                {
+                    messageId: 'map'
+                }
+            ],
+            output: '[4, 8].map(function(n) { return n*n; });'
+        }
+    ]
+});

c8fb14d80c54553c1a30d6796edfc828c8bed90b
Zlata Kotlova <zlata.kotlova@mail.ru>
2019-09-28 18:55:28 +0300

    'Refactoring'

--- tests/lib/rules/map.js
+++ tests/lib/rules/map.js
@@ -5,9 +5,10 @@ var ruleTester = new RuleTester();
 
 ruleTester.run('map', rule, {
     valid: [
-        '_.map({ \'a\': 4, \'b\': 8 }, function(n) { return n*n; });',
+        '_.map({ "a": 4, "b": 8 }, function(n) { return n*n; });',
+        '_.map(1+2, function(n) { return n*n; });',
         '[1,2,3].map(function(n) { return n*n; });',
-        'var obj = { \'a\': 4, \'b\': 8 }; _.map(obj, function(n) { return n*n; });',
+        'var obj = { "a": 4, "b": 8 }; _.map(obj, function(n) { return n*n; });',
         'var arr = [1,2,3]; arr.map(function(n) { return n*n; });'
     ],
     invalid: [
@@ -40,14 +41,14 @@ ruleTester.run('map', rule, {
                 `function getArr() { return [1,2,3]; } \nvar getArrResult = getArr();\nArray.isArray(getArrResult) ? getArrResult.map(function(n) { return n*n; }) : _.map(getArrResult, function(n) { return n*n; });`
         },
         {
-            code: 'function getObj() { return { \'a\': 4, \'b\': 8 }; } _.map(getObj(), function(n) { return n*n; });',
+            code: 'function getObj() { return { "a": 4, "b": 8 }; } _.map(getObj(), function(n) { return n*n; });',
             errors: [
                 {
                     messageId: 'lodashMapToNative'
                 }
             ],
             output:
-                `function getObj() { return { \'a\': 4, \'b\': 8 }; } \nvar getObjResult = getObj();\nArray.isArray(getObjResult) ? getObjResult.map(function(n) { return n*n; }) : _.map(getObjResult, function(n) { return n*n; });`
+                `function getObj() { return { "a": 4, "b": 8 }; } \nvar getObjResult = getObj();\nArray.isArray(getObjResult) ? getObjResult.map(function(n) { return n*n; }) : _.map(getObjResult, function(n) { return n*n; });`
         },
         {
             code: 'function getArr() { return [1,2,3]; } var res = _.map(getArr(), function(n) { return n*n; });',
@@ -58,6 +59,35 @@ ruleTester.run('map', rule, {
             ],
             output:
                 `function getArr() { return [1,2,3]; } var res = Array.isArray(getArr()) ? getArr().map(function(n) { return n*n; }) : _.map(getArr(), function(n) { return n*n; });`
+        },
+        {
+            code: '_.map(1 || [], function(n) { return n*n; });',
+            errors: [
+                {
+                    messageId: 'lodashMapToNative'
+                }
+            ],
+            output:
+                `Array.isArray(1 || []) ? (1 || []).map(function(n) { return n*n; }) : _.map(1 || [], function(n) { return n*n; });`
+        },
+        {
+            code: '_.map(1 + 2 === 3 ? [] : {}, function(n) { return n*n; });',
+            errors: [
+                {
+                    messageId: 'lodashMapToNative'
+                }
+            ],
+            output:
+                `Array.isArray(1 + 2 === 3 ? [] : {}) ? (1 + 2 === 3 ? [] : {}).map(function(n) { return n*n; }) : _.map(1 + 2 === 3 ? [] : {}, function(n) { return n*n; });`
+        },
+        {
+            code: 'var res = {} || []; _.map(res, function(n) { return n*n; });',
+            errors: [
+                {
+                    messageId: 'lodashMapToNative'
+                }
+            ],
+            output: 'var res = {} || []; Array.isArray(res) ? res.map(function(n) { return n*n; }) : _.map(res, function(n) { return n*n; });'
         },
     ]
 });
